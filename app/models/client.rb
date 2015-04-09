28844f56173342fb2ae0235921a9431214e6d5f2
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2015-04-09 16:53:25 +1000

    Return project_ids under client json

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,6 @@ class Client < ActiveRecord::Base
   has_many :projects
 
   def as_json(options = {})
-    { id: id, name: name, amount_unbilled: rand(10000).to_i, projects: projects }
+    { id: id, name: name, amount_unbilled: rand(10000).to_i, project_ids: project_ids }
   end
 end
